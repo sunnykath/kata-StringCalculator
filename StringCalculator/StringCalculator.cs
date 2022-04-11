ec32f4aab75ecf21484b7c3c0c88aa0f0c921f56
Suyash <suyash.kathuria7@gmail.com>
2022-04-12 10:26:48 +1200

    Test: Negative numbers not allowed and throws exception | REFACTOR

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -6,9 +6,9 @@ namespace kata_string_calculator
     public class StringCalculator
     {
         private const string OptionalArg = "//";
+        private const int CustomDelimiterIndex = 2;
         
-        private char _customDelimiter;
-        private readonly char[] _separators = {',', '\n'};
+        private char[] _separators = {',', '\n'};
         private string _inputString;
         public int Add(string stringInput)
         {
@@ -16,36 +16,35 @@ namespace kata_string_calculator
             
             if (_inputString.Contains(OptionalArg))
             {
-                UpdateCustomDelimiter(stringInput);
+                UpdateDelimitersWithCustomDelimiter(stringInput);
                 _inputString = _inputString.Substring(4);
             }
 
             return CalculateSumOfString();
         }
 
-        private void UpdateCustomDelimiter(string stringInput)
+        private void UpdateDelimitersWithCustomDelimiter(string stringInput)
         {
-            _customDelimiter = stringInput[2];
+            _separators = _separators.Append(stringInput[CustomDelimiterIndex]).ToArray();
         }
         
         private int CalculateSumOfString()
         {
-            var customSeparators = _separators;
-            if (_customDelimiter != 0)
-            {
-                customSeparators = customSeparators.Append(_customDelimiter).ToArray();
-            }
+            var numbers = _inputString.Split(_separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
 
-            var numbers = _inputString.Split(customSeparators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            CheckForNegatives(numbers);
+            
+            return numbers.Sum();
+        }
 
+        private void CheckForNegatives(int[] numbers)
+        {
             var negativeNumbers = numbers.Where(number => number < 0).ToArray();
 
             if (negativeNumbers.Length != 0)
             {
                 throw new Exception($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
             }
-            
-            return numbers.Sum();
         }
     }
 }
