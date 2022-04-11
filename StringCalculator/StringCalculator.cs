608b4fc09b5d14c399b9088e53e987215354ad74
Suyash <suyash.kathuria7@gmail.com>
2022-04-11 16:16:01 +1200

    Test: Returns a number when given a string | PASS

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -2,6 +2,9 @@ namespace kata_string_calculator
 {
     public class StringCalculator
     {
-        
+        public int Calculate(string stringInput)
+        {
+            return 0;
+        }
     }
 }
