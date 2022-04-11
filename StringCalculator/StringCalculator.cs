f8367edee33ba39b30d53e5689a1cc0027a61a36
Suyash <suyash.kathuria7@gmail.com>
2022-04-11 16:21:09 +1200

    Test: Returns the sum of the numbers that were passed in | PASS

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -4,9 +4,17 @@ namespace kata_string_calculator
     {
         public int Calculate(string stringInput)
         {
-            var parseSuccess = int.TryParse(stringInput, out var number);
-            
-            return parseSuccess ? number : 0;
+            if (stringInput.Contains(','))
+            {
+                var numbers = stringInput.Split(',');
+                var sum = int.Parse(numbers[0]) + int.Parse(numbers[1]);
+                return sum;
+            }
+            else
+            {
+                var parseSuccess = int.TryParse(stringInput, out var number);
+                return parseSuccess ? number : 0;
+            }
         }
     }
 }
