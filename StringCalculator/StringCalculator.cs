e0fb1bcf14b1122407d4301a54c24c066b40d72a
Suyash <suyash.kathuria7@gmail.com>
2022-04-11 13:20:13 +1200

    Revert "Test: Returns the sum of the numbers that were passed in | PASS"
    This reverts commit 9d82cfcdb3f4c69c2c09a6e655db506ce5189b4b.

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 
 namespace kata_string_calculator
 {
@@ -7,17 +6,9 @@ namespace kata_string_calculator
     {
         public int Calculate(string s)
         {
-            if (s.Contains(','))
-            {
-                var numbers = s.Split(',');
-                var sum = int.Parse(numbers[0]) + int.Parse(numbers[1]);
-                return sum;
-            }
-            else
-            {
-                var parseSuccess = int.TryParse(s, out var number);
-                return parseSuccess ? number : 0;
-            }
+            var parseSuccess = int.TryParse(s, out var number);
+            
+            return parseSuccess ? number : 0;
         }
     }
 }
