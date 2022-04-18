bea0213e9c7bab75a72b64a34ecfe2d0d51b9f56
Suyash <suyash.kathuria7@gmail.com>
2022-04-19 11:24:21 +1200

    Updated StringCalculator to use the new interfaces

--- StringCalculator/StringCalculator.cs
+++ StringCalculator/StringCalculator.cs
@@ -1,72 +1,31 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text.RegularExpressions;
+using kata_string_calculator.Delimiters;
 
 namespace kata_string_calculator
 {
     public class StringCalculator
     {
-        private const string OptionalArg = "//";
-        private const int CustomDelimiterIndex = 2;
-        private const char StartOfCustomDelimiter = '[';
-        private const string CustomDelimiterRegex = "(?<=\\[).+?(?=\\])";
+        private readonly IValidation _validator;
+        private readonly StringsToNumbers _stringConversion;
+        
+        public StringCalculator(IDelimiters delimitersHandler, IValidation validator)
+        {
+            _stringConversion = new StringsToNumbers(delimitersHandler);
+            _validator = validator;
+        }
 
-        private string[] _separators = {",", "\n"};
-        private string _numbersInStringInput = null!;
         public int Add(string stringInput)
         {
-            if (stringInput.Contains(OptionalArg))
-            {
-                var endOfCustomDelimiter = stringInput.IndexOf('\n');
-                UpdateDelimitersWithCustomDelimiter(stringInput[..endOfCustomDelimiter]);
-                _numbersInStringInput = stringInput[endOfCustomDelimiter..];
-            }
-            return GetSumOfString();
+            var numbers = _stringConversion.GetNumbersFromString(stringInput);
+
+            return GetValidSumOfNumbers(numbers);
         }
 
-        private void UpdateDelimitersWithCustomDelimiter(string stringInput)
+        private int GetValidSumOfNumbers(int[] allNumbers)
         {
-            List<string> customDelimiters = new ();
-            
-            if (stringInput.Contains(StartOfCustomDelimiter))
-            {
-                var delimiterStrings = Regex.Matches(stringInput, CustomDelimiterRegex);
-                customDelimiters.AddRange(delimiterStrings.Select(d => d.ToString()));
-            }
-            else
-            {
-                customDelimiters.Add(stringInput[CustomDelimiterIndex] + "");
-            }
-            
-            customDelimiters.AddRange(_separators);
-            _separators = customDelimiters.ToArray();
-        } 
-        
-        private int GetSumOfString()
-        {
-            var initialNumbers = _numbersInStringInput.Split(_separators, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            var validNumbers = _validator.GetValidNumbers(allNumbers);
 
-            CheckForNegatives(initialNumbers);
-
-            var numbers = GetNumbersBelowAThousand(initialNumbers);
-            
-            return numbers.Sum();
-        }
-        
-        private static void CheckForNegatives(int[] numbers)
-        {
-            var negativeNumbers = numbers.Where(number => number < 0).ToArray();
-
-            if (negativeNumbers.Length != 0)
-            {
-                throw new Exception($"Negatives not allowed: {string.Join(", ", negativeNumbers)}");
-            }
-        }
-
-        private static IEnumerable<int> GetNumbersBelowAThousand(IEnumerable<int> numbers)
-        {
-            return numbers.Where(number => number < 1000);
+            return validNumbers.Sum();
         }
     }
 }
